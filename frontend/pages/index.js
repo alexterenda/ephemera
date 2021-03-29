a64e8047217a9af2475598d0b026aef479cd8842
Alex Terenda <alexterenda@gmail.com>
2021-03-29 09:28:42 +0100

    Added ProductList and Product components

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,1 @@
-import Page from "../components/Page";
-
-export default function IndexPage() {
-  return (
-    <>
-      <p>Hello!</p>
-      <p>I am a child</p>
-    </>
-  );
-}
+export { default } from './products';
